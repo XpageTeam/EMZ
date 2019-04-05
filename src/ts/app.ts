03ec731d2fdbb300764dd7bb7ce11602bbe82053
danil.khandys <danil.khandys@xpage.ru>
2019-04-05 17:01:10 +0500

    1232

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,29 +1,29 @@
 class App {
 	// private document: any = document;
 
-	public static domReady(callback: any) {
+	public static domReady(callback: any): void {
 		document.addEventListener("DOMContentLoaded", callback)
 	}
 
-	public static getElements(selector: string): any{
+	public static getElements(selector: string): Array<HTMLElement>{
 		const elements: any = document.querySelectorAll(selector);
 
 		return elements.length ? elements : []
 	}
 
-	public static getElement(selector: string): any{
+	public static getElement(selector: string): HTMLElement{
 		const element: any = document.querySelector(selector);
 
 		return element
 	}
 
-	protected elementsGetter(selector: string){
+	protected elementsGetter(selector: string): Array<HTMLElement>{
 		const elements: any = document.querySelectorAll(selector);
 
 		return elements.length ? elements : []
 	}
 
-	protected each(elements: Array<any>, callback: any){
+	protected each(elements: Array<HTMLElement>, callback: any): void{
 
 		for (let i in elements){
 			const el = elements[i];
@@ -36,15 +36,27 @@ class App {
 
 class EventListener extends App{
 	private _selector: string
-	private _els: Array<any>
+	private _els: Array<HTMLElement>
 
-	constructor (selector: string){
+	constructor (selector: Array<HTMLElement>)
+	constructor (selector: string)
+	constructor (selector: any){
 		super()
 
-		this._els = this.elementsGetter(selector)
+		switch (typeof(selector)){
+			case "object":
+				this._els = selector
+			break;
+			case "string": 
+				this._els = this.elementsGetter(selector)
+			break;
+
+			default:
+				console.error('Invalid selector: ${selector}')
+		}
 	}
 
-	public add(event: string, callback: any){
+	public add(event: string, callback: any): EventListener{
 
 		this.each(this._els, function(){
 			this.addEventListener(event, callback().bind(this))
